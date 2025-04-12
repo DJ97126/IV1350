6d64a9fa19f76a26eabe1a41db50af581a9f406e
yyIntsuki <intsuki.yz@gmail.com>
2025-04-12 16:48:36 +0200

    added a simple test example for later use.

---
+++ seminar_3/test/view/ViewTest.java
@@ -0,0 +1,12 @@
+package view;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ViewTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 1+1);
+    }
+}
