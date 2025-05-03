217aaa117575a2a11c7113fbc013eae279717085
louai20 <alayoubi@kth.se>
2025-05-03 14:20:37 +0200

    small fixes

--- seminar_3/src/utils/StringUtils.java
+++ seminar_3/src/utils/StringUtils.java
@@ -2,6 +2,9 @@ package utils;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class StringUtils {
     public StringUtils() {}
@@ -9,10 +12,13 @@ public class StringUtils {
     /**
      * Formats a BigDecimal to a string with two decimal places, replacing the decimal point with a colon.
      * 
-     * @param number
+     * @param number The decimal number to format.
      * @return The formatted string with a colon instead of a decimal point.
      */
     public static String formatBigDecimalToColon(BigDecimal number) {
-        return String.format("%.2f", number.setScale(2, RoundingMode.HALF_UP)).replace('.', ':');
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        symbols.setDecimalSeparator(':');
+        DecimalFormat formatter = new DecimalFormat("0.00", symbols);
+        return formatter.format(number.setScale(2, RoundingMode.HALF_UP));
     }
 }
