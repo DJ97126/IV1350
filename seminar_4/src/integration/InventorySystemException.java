935e05119808a041ba8476c39bcc3934872ea6bf
yyIntsuki <intsuki.yz@gmail.com>
2025-05-14 00:49:29 +0200

    fixed an incorrect indentation in javadoc comment.

--- seminar_4/src/integration/InventorySystemException.java
+++ seminar_4/src/integration/InventorySystemException.java
@@ -1,17 +1,17 @@
 package integration;
 
 /**
- * Thrown when the inventory database cannot be reached or is unavailable.
+ * Thrown when the inventory database cannot be reached or is unavailable. 
  * This simulates a database failure, such as the server not running.
  */
 public class InventorySystemException extends RuntimeException {
-	
-    /**
-     * Creates a new instance representing the condition described in the specified message.
+
+	/**
+	 * Creates a new instance representing the condition described in the specified message.
 	 * 
-     * @param message A message that describes what went wrong.
-     */
-    InventorySystemException(String message) {
-        super(message);
-    }
+	 * @param message A message that describes what went wrong.
+	 */
+	InventorySystemException(String message) {
+		super(message);
+	}
 }
